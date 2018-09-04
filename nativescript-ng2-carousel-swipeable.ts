9a5a3b221cf411482b26f0e41ec4375494fed941
fdiazaguirre <fdiazaguirre@gmail.com>
2018-09-04 21:35:22 +0200

    Remove comment

--- nativescript-ng2-carousel-swipeable.ts
+++ nativescript-ng2-carousel-swipeable.ts
@@ -56,7 +56,6 @@ export class CarouselDirective implements AfterViewInit {
             this.initControls();
         }
         this.initAutoPlay();
-        console.log(`${this.currentElementHiglhtColor}`)
     }
 
     /**
